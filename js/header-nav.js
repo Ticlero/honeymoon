3ad1331193d44d19bfe330f0059687157bdbfde3
Ticlero <cbiscuit@naver.com>
2020-05-26 22:56:34 +0900

    cancun into-description add

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,10 +1,10 @@
 const navEle = document.querySelector(".icon__nav i");
+const overlayEle = document.querySelector(".overlay");
+const body_overlay_Ele = document.querySelector(".overlay");
+const nav = document.querySelector(".nav-list");
 
 navEle.addEventListener("click", (event) => {
-  const body_overlay_Ele = document.querySelector(".overlay");
   body_overlay_Ele.classList.toggle("body-overlay");
-
-  const nav = document.querySelector(".nav-list");
   nav.classList.toggle("show-nav");
 
   if (nav.classList.contains("show-nav")) {
@@ -16,5 +16,23 @@ navEle.addEventListener("click", (event) => {
     nav.childNodes[1].style.animation =
       "hide-menu-lists 0.5s ease-in-out forwards";
     nav.style.animation = "dropup 0.5s ease-in-out forwards";
+  }
+});
+
+overlayEle.addEventListener("click", (e) => {
+  if (body_overlay_Ele.classList.contains("body-overlay")) {
+    body_overlay_Ele.classList.toggle("body-overlay");
+    nav.classList.toggle("show-nav");
+
+    if (nav.classList.contains("show-nav")) {
+      nav.style.display = "block";
+      nav.childNodes[1].style.animation =
+        "show-menu-lists 0.5s ease-in-out forwards";
+      nav.style.animation = "dropdown 0.5s ease-in-out forwards";
+    } else {
+      nav.childNodes[1].style.animation =
+        "hide-menu-lists 0.5s ease-in-out forwards";
+      nav.style.animation = "dropup 0.5s ease-in-out forwards";
+    }
   }
 });
